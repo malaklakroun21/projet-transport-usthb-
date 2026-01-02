3fa56e687564670636c493fa21da530b1fdd35a2
Malak <mlakrou280@gmail.com>
2026-01-02 11:34:53 +0100

    expedition updated avec section admin et reformulations de models

--- delivery_management/apps/logistics/admin.py
+++ delivery_management/apps/logistics/admin.py
@@ -1,3 +1,34 @@
 from django.contrib import admin
-
+from .models import Expedition 
+from .models import Chauffeur, Vehicule, Tournee, TypeService, Destination
 # Register your models here.
+
+@admin.register(Expedition)
+class ExpeditionAdmin(admin.ModelAdmin):
+    list_display = ('numero_suivi', 'client', 'status', 'type_service', 'destination', 'tournee', 'montant_total', 'date_creation')
+    list_filter = ('status', 'type_service', 'date_creation')
+    search_fields = ('numero_suivi', 'client__nom', 'destination__ville')
+
+@admin.register(Chauffeur)
+class ChauffeurAdmin(admin.ModelAdmin):
+    list_display = ('nom', 'telephone')
+    search_fields = ('nom',)
+
+@admin.register(Vehicule)
+class VehiculeAdmin(admin.ModelAdmin):
+    list_display = ('immatriculation', 'type')
+    search_fields = ('immatriculation',)
+
+@admin.register(Tournee)
+class TourneeAdmin(admin.ModelAdmin):
+    list_display = ('date', 'chauffeur', 'vehicule')
+    list_filter = ('date',)
+
+@admin.register(Destination)
+class DestinationAdmin(admin.ModelAdmin):
+    list_display = ('ville', 'pays', 'code_postal')
+    search_fields = ('ville', 'pays')
+
+@admin.register(TypeService)
+class TypeServiceAdmin(admin.ModelAdmin):
+    list_display = ('nom',)
--- delivery_management/delivery_management/settings.py
+++ delivery_management/delivery_management/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'clients',
+    'apps.clients',
+    'apps.logistics',
 ]
 
 MIDDLEWARE = [
