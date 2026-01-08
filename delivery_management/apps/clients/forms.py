309395d4e1966303d622700cdd17991064b8bcb6
Malak <mlakrou280@gmail.com>
2026-01-08 19:04:38 +0100

    Revert "Merge pull request #17 from malaklakroun21/oussama1"
    This reverts commit eacc46acd04e9499a16e6335dcdf3e7ee212d94e, reversing
    changes made to 561582f69e418dc4ac334f87afb25096e1b43414.

--- delivery_management/apps/clients/forms.py
+++
@@ -1,8 +0,0 @@
-from django.forms import ModelForm 
-from .models import Clients
-
-class ClientsForm(ModelForm):
-    class Meta:
-        model = Clients
-        fields = '__all__'
-        exclude = ['created_at']
--- delivery_management/apps/clients/models.py
+++ delivery_management/apps/clients/models.py
@@ -1,25 +1,9 @@
 from django.db import models
 
-class Clients(models.Model):
-
-    CLIENT_TYPE_CHOICES = [
-        ('Actif', 'Actif'),
-        ('Inactif', 'Inactif'),
-    ]
-
-    code_client = models.CharField(max_length=20, unique=True)
 class Client(models.Model):
     code_client = models.CharField(max_length=50, unique=True)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
-    address = models.TextField()
-
-    client_type = models.CharField(
-        max_length=50,
-        choices=CLIENT_TYPE_CHOICES
-    )
-
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default=0)
     phone = models.CharField(max_length=20)
     address = models.CharField(max_length=255)
     client_type = models.CharField(max_length=50)
